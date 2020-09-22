201a1591d53b44429f90c5c64b4c70076ee67d7b
Giulio Russo <me@giuuliorusso.me>
2020-09-22 03:18:06 +0200

    Initial commit

---
+++ Shared/SpaceXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXApp.swift
+//  Shared
+//
+//  Created by Giulio Russo on 22/09/20.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
